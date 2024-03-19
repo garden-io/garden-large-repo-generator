ec1e6fb3013dda74469603ca1cb2cdd568a6dc77
Vladimir Vagaytsev <vladimir.vagaitsev@gmail.com>
2024-03-19 13:00:59 +0100

    chore: generate large number of files

--- generate.sh
+++ generate.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 
-for i in {1..50}; do
+set -e
+
+cd spring-boot/deployment/pulumi
+echo "Creating large number of files..."
+for i in {1..1000}; do
+  echo $i
+  cp "package-lock.json" "dummy-file-${i}"
+done
+
+cd ../../..
+echo "Creating large number of spring-boot module copies..."
+for i in {1..100}; do
   echo $i
   cp -R spring-boot "spring-boot-${i}"
   yq -i '.name = "spring-boot-'$i'"' spring-boot-$i/spring-boot.garden.yml
